a6417b9b27221a955821017163601668aaf6efc1
ddai <daichao0805@163.com>
2020-03-14 23:22:50 +0800

    update

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <div id="app">
-    <el-radio-button  label="1">备选项</el-radio-button>
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple-dark">a</div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import 'element-ui/lib/theme-chalk/index.css';
 // 单个引入, 在 node_modules/element-ui/packages/ 打断点
 import ElRadioButton from "element-ui/packages/radio/src/radio-button.vue";
+import ElRow from "element-ui/packages/row/index";
+import ElCol from "element-ui/packages/col/index";
 
 export default {
   name: "App",
+  /* eslint-disable vue/no-unused-components */
   components: {
-   ElRadioButton,
+    ElRadioButton,
+    ElRow,
+    ElCol
   }
 };
 </script>
 
 <style>
 </style>
+/* eslint-enable no-alert, no-console */
